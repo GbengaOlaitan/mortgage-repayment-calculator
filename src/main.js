b59858efeb0396e9c68a4951aee0aeee870db0c1
hazeem <olaitanhazeem@gmail.com>
2025-05-30 14:03:12 +0100

    validate the calculator form for input

--- src/main.js
+++ src/main.js
@@ -8,7 +8,23 @@ const resultSection = document.querySelector(".js-result-section");
 const displaySection = document.querySelector(".js-display-section");
 
 calcButton.addEventListener("click", () => {
-  displaySection.classList.add("hidden");
+  //validate the calculator form
+    if(mAmount.value == '' && mTerm.value == '') {
+    alert("Fill the Mortgage Form")
+    return
+  }
+
+  if(mAmount.value == '') {
+    alert('fill Mortage Amount')
+    return;
+  } else if (mTerm.value == '') {
+    alert('Specify loan term')
+    return
+  }
+
+
+  if(mAmount.value !== '' && mTerm.value !== '') {
+      displaySection.classList.add("hidden");
   resultSection.classList.remove("hidden");
   const interestRate = Number(interest.value / 100);
   const mAmountValue = Number(mAmount.value);
@@ -25,6 +41,7 @@ calcButton.addEventListener("click", () => {
   const tResult = document.querySelector(".js-total-result");
   const totalRepayment = interestAmount + mAmountValue;
   tResult.innerHTML = `<span>&#x20A6</span>${totalRepayment}`;
+  }
 });
 
 //Reset calculator
