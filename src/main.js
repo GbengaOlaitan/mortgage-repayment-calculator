aa7583c3d324040619e1d56e76be209264e9e605
hazeem <olaitanhazeem@gmail.com>
2025-05-31 17:12:00 +0100

    added on focus functionality to the input field

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 "use strict";
 const calcButton = document.querySelector(".js-calculate");
 const mAmount = document.querySelector(".js-m-amount");
-const mTerm = document.querySelector(".js-m-term");
+const mTerm = document.querySelector(".js-mt-term");
 const interest = document.querySelector(".js-interest");
 const mResult = document.querySelector(".js-result");
 const resultSection = document.querySelector(".js-result-section");
@@ -31,15 +31,19 @@ calcButton.addEventListener("click", () => {
   const mTermValue = Number(mTerm.value);
   const interestPa = interestRate * mTermValue;
   const interestAmount = mAmountValue * interestPa;
-  const monthlyRepayments = (
+
+  let monthlyRepayments = Number((
     (interestAmount + mAmountValue) /
     mTermValue /
     12
-  ).toFixed(2);
-  const mRepayments = `<span>&#x20A6</span>${monthlyRepayments}`;
+  ).toFixed(2));
+
+  const formattedMonthlyRepayments = monthlyRepayments.toLocaleString('en-US');
+
+  const mRepayments = `<span>&#x20A6</span>${formattedMonthlyRepayments}`;
   mResult.innerHTML = mRepayments;
   const tResult = document.querySelector(".js-total-result");
-  const totalRepayment = interestAmount + mAmountValue;
+  const totalRepayment = (interestAmount + mAmountValue).toLocaleString('en-US');
   tResult.innerHTML = `<span>&#x20A6</span>${totalRepayment}`;
   }
 });
@@ -52,3 +56,53 @@ clearButton.addEventListener("click", () => {
     displaySection.classList.remove("hidden");
   resultSection.classList.add("hidden");
 });
+
+//on focus
+
+const maELement = document.querySelector(".js-m-amount");
+const maElementContainer = document.querySelector(".js-ma-ElementContainer");
+const maTextBackground = document.querySelector(".js-ma-TextBackground");
+
+maELement.addEventListener("focus", () => {
+  maElementContainer.classList.add('border-limeColor');
+  maTextBackground.classList.add('bg-limeColor');
+});
+
+maELement.addEventListener("blur", () => {
+  maElementContainer.classList.remove('border-limeColor');
+  maTextBackground.classList.remove('bg-limeColor');
+});
+
+// mortgage term
+
+const mtElement = document.querySelector(".js-mt-term");
+const mtElementContainer = document.querySelector(".js-mt-ElementContainer");
+const mtTextBackground = document.querySelector(".js-mt-TextBackground");
+
+mtElement.addEventListener("focus", () => {
+  mtElementContainer.classList.add('border-limeColor');
+  mtTextBackground.classList.add('bg-limeColor');
+});
+
+mtElement.addEventListener("blur", () => {
+  mtElementContainer.classList.remove('border-limeColor');
+  mtTextBackground.classList.remove('bg-limeColor');
+});
+
+//interest rate
+
+const irElement = document.querySelector(".js-interest");
+const irElementContainer = document.querySelector(".js-ir-ElementContainer");
+const irTextBackground = document.querySelector(".js-ir-TextBackground");
+
+irElement.addEventListener("focus", () => {
+  irElementContainer.classList.add('border-limeColor');
+  irTextBackground.classList.add('bg-limeColor');
+});
+
+irElement.addEventListener("blur", () => {
+  irElementContainer.classList.remove('border-limeColor');
+  irTextBackground.classList.remove('bg-limeColor');
+});
+
+
